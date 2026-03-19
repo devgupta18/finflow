f4d04eb3e567c602ff335709b22329ece7e79045
devgupta18 <gupta.dev2002@gmail.com>
2026-03-20 02:33:19 +0530

    feat: add custom thread pool executor for async processing

---
+++ src/main/java/com/example/finflow/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.example.finflow.config;
+
+import com.example.finflow.exception.AsyncExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements  AsyncConfigurer {
+    @Bean
+    public Executor fraudDetectionExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(3);
+        taskExecutor.setMaxPoolSize(6);
+        taskExecutor.setQueueCapacity(10);
+        taskExecutor.setThreadNamePrefix("fraud-detection");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+}
